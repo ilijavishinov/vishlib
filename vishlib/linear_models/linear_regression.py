c72173152f8943bd599d63eb3753eebbee872b13
ilijavishinov@ryzen4800h <ilijavishinovprofessional@gmail.com>
2021-04-28 21:51:35 +0200

    lin. reg. grad. desc. without bias implemented

--- vishlib/linear_models/linear_regression.py
+++ vishlib/linear_models/linear_regression.py
@@ -1,20 +1,14 @@
 import numpy as np
 
-
 class LinearRegression:
     
-    
     def __init__(self, max_iter):
-        
         # constructor args
         self.max_iter = max_iter
-        
-        
-    
-    #-----------------------------------------------------------------
+
+    # -----------------------------------------------------------------
     
     def fit(self, X_train: np.ndarray, y_train: np.ndarray):
-        
         # initialize weights
         weights = np.zeros(X_train.shape[1])
         num_instances = X_train.shape[0]
@@ -25,16 +19,23 @@ class LinearRegression:
             
             # make predictions and calculate loss on train set
             y_pred = np.multiply(X_train, weights).sum(axis = 1)
-            loss = np.sum((y_train - y_pred)**2)/(2*num_instances)
+            loss = np.sum((y_train - y_pred) ** 2) / (2 * num_instances)
             
             # update weights
+            updatad_weights = np.array([np.nan] * weights.size)
             for weight_idx in range(weights.size):
-                # weights[weight_idx] =
-                pass
-            pass
-        
                 
+                # calculate update component (partial derivative mul. by learning rate)
+                partial_derivative = 0
+                for row_i, y_i, y_pred_i in zip(X_train, y_train, y_pred):
+                    partial_derivative += (y_pred_i - y_i) * row_i[weight_idx]
+                partial_derivative /= num_instances
+                update_component = partial_derivative * learning_rate
                 
+                updatad_weights[weight_idx] = weights[weight_idx] - update_component
+            weights = updatad_weights
+                
+            
                 
                 
                 
